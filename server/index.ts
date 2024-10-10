200794f8b20472d8f6ff9ae2ee79fceb828f0470
jolbol1 <github@jamesshopland.com>
2024-10-10 12:00:00 +0000

    step 3: hono error handling

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,41 @@
+import { type ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errResponse =
+      err.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          error: err.message,
+          isFormError:
+            err.cause && typeof err.cause === "object" && "form" in err.cause
+              ? err.cause.form === true
+              : false,
+        },
+        err.status,
+      );
+    return errResponse;
+  }
+
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Interal Server Error"
+          : (err.stack ?? err.message),
+    },
+    500,
+  );
 });
 
 export default app;
---
+++ shared/types.ts
@@ -0,0 +1,10 @@
+export type SuccessResponse<T = void> = {
+  success: true;
+  message: string;
+} & (T extends void ? {} : { data: T });
+
+export type ErrorResponse = {
+  success: false;
+  error: string;
+  isFormError?: boolean;
+};
